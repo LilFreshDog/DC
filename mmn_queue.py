dbd3a7d973b7be1c96622827c97753d347fe90fd
Giorgio Rengucci <4483986@studenti.unige.it>
2022-11-30 16:01:14 +0100

    te lo pusho tutto

--- mmn_queue.py
+++ mmn_queue.py
@@ -21,6 +21,8 @@ class MMN(Simulation):
         self.running = [None] * n 
         #self.queue = collections.deque()  # FIFO queue of the system
         self.queues = [collections.deque() for i in range(n)]  # FIFO queues of the system
+        self.queue_counter = [0] * n # average length of the queues
+        self.total_jobs = 0  # total number of jobs ever entered in the system (?)
         self.arrivals = {}  # dictionary mapping job id to arrival time
         self.completions = {}  # dictionary mapping job id to completion time
         self.lambd = lambd # arrival rate
@@ -33,12 +35,12 @@ class MMN(Simulation):
             self.d = n
         self.arrival_rate = lambd * n
         self.completion_rate = mu 
-        self.schedule(expovariate(lambd), Arrival(0, 0))
+        self.schedule(expovariate(lambd), Arrival(0))
     
     def get_min_queue(self) -> int:
         """ returns id of the shortest length queue selected from a subset `d` (supermarket model) """
         sampled_queues_ids = sample(range(self.n), self.d)
-        min_queue_id = [0]
+        min_queue_id = 0
         for i in sampled_queues_ids:
             if len(self.queues[i]) < len(self.queues[min_queue_id]):
                 min_queue_id = i
@@ -46,12 +48,12 @@ class MMN(Simulation):
     
     def print_queue(self):
         """ print number of jobs in each queue """
-        print([self.queue_len(i) for i in range(self.n)], end="\r")
+        print([self.queue_len(i) for i in range(self.n)], end="\n")
 
-    def schedule_arrival(self, job_id, queue_id):
+    def schedule_arrival(self, job_id):
         # schedule the arrival following an exponential distribution, to compensate the number of queues the arrival
         # time should depend also on "n"
-        self.schedule(expovariate(self.arrival_rate), Arrival(job_id, queue_id))
+        self.schedule(expovariate(self.arrival_rate), Arrival(job_id))
 
     def schedule_completion(self, job_id, queue_id):
         # schedule the time of the completion event
@@ -63,28 +65,29 @@ class MMN(Simulation):
 
 class Arrival(Event):
 
-    def __init__(self, job_id: int, queue_id: int):
+    def __init__(self, job_id: int):
         self.id = job_id
-        self.queue_id = queue_id
 
     def process(self, sim: MMN):
         # set the arrival time of the job
         sim.arrivals[self.id] = sim.t
+        sim.total_jobs += 1
         
-        # queue_id = sample(range(len(sim.queues)), 1)[0] # uncomment to select a random queue
+        # queue_id = randint(0, sim.n) # uncomment to select a random queue
         queue_id = sim.get_min_queue() # uncomment to select the shortest queue (supermarket model)
+        sim.queue_counter[queue_id] += 1
 
         # if there is no running job in the current queue, start the job
-        if sim.running[self.queue_id] is None:
-            sim.running[self.queue_id] = self.id
-            sim.schedule_completion(self.id, self.queue_id)
+        if sim.running[queue_id] is None:
+            sim.running[queue_id] = self.id
+            sim.schedule_completion(self.id, queue_id)
         # otherwise put the job into the queue
         else:
             #choose the queue with the shortest length randomly
             sim.queues[queue_id].append(self.id)
 
         # schedule the arrival of the next job
-        sim.schedule_arrival(self.id + 1, queue_id)
+        sim.schedule_arrival(self.id + 1)
 
 class Completion(Event):
     def __init__(self, job_id, queue_id):
@@ -93,7 +96,8 @@ class Completion(Event):
 
     def process(self, sim: MMN):
         assert sim.running[self.queue_id] is not None
-        # DEBUG # sim.print_queue() if sim.n <= 10 else None
+        # DEBUG 
+        sim.print_queue() if sim.n <= 10 else None
         # set the completion time of the running job
         sim.completions[sim.running[self.queue_id]] = sim.t
         # if the queue is not empty
@@ -106,7 +110,7 @@ class Completion(Event):
             sim.running[self.queue_id] = None
 
 
-def show_graphs(result_queues, lambdas):
+def show_graphs(result_queues: list, average_queue_lengths: list, lambdas: list, max_t: int):
     import matplotlib.pyplot as plt
     import numpy as np
     import collections
@@ -114,12 +118,13 @@ def show_graphs(result_queues, lambdas):
     print("Creating graphs...")
     counters = []
 
-    for i, queue in enumerate(result_queues):
-        queue_lengths = [len(queue) for queue in result_queues[i] if len(queue) > 0]  
-        counter = collections.Counter(queue_lengths)
+    for i, queue in enumerate(average_queue_lengths):
+        #queue_lengths = [len(queue) for queue in result_queues[i] if len(queue) > 0]  
+        av_queue_lengths = [length/max_t for length in average_queue_lengths[i] if len(queue) > 0]
+        counter = collections.Counter(av_queue_lengths)
         counter = collections.OrderedDict(sorted(counter.items()))
         for key,value in counter.items():
-            counter[key] = value/len(queue_lengths)
+            counter[key] = value/len(av_queue_lengths)
         counters.append(counter)
 
     plt.title("Distribution of queue lengths")
@@ -147,6 +152,7 @@ def main():
     args = parser.parse_args()
 
     result_queues = []
+    average_queues_lenghts = []
     lambdas_x = [0.5, 0.90,  0.95, 0.99]
     for lambd_x in lambdas_x:
         sim = MMN(lambd_x, args.mu, args.n, args.d)
@@ -163,9 +169,12 @@ def main():
                 writer.writerow([args.lambd, args.mu, args.max_t, W])
 
         result_queues.append(sim.queues)
-
+        average_queues_lenghts.append(sim.queue_counter)
+        print(sim.queue_counter)
+        print([counter/sim.total_jobs for counter in sim.queue_counter], end=" ")
+        print()
     if args.graph:
-        show_graphs(result_queues, lambdas_x)
+        show_graphs(result_queues, average_queues_lenghts, lambdas_x, sim.total_jobs)
 
 
 
